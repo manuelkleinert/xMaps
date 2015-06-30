02105d66aa91403c761a701a24b0065d747fd770
Manuel <info@xpager.ch>
2015-06-30 14:03:54 +0200

    New Version

--- xMaps.js
+++ xMaps.js
@@ -1,30 +1,5 @@
 /*#####################################################################################################################
-                                                                                                              
-                    PPPPPPPPPPPPPPPPP                                                                              
-                    P::::::::::::::::P                                                                             
-                    P::::::PPPPPP:::::P                                                                            
-                    PP:::::P     P:::::P                                                                           
-xxxxxxx      xxxxxxx  P::::P     P:::::Paaaaaaaaaaaaa     ggggggggg   ggggg    eeeeeeeeeeee    rrrrr   rrrrrrrrr   
- x:::::x    x:::::x   P::::P     P:::::Pa::::::::::::a   g:::::::::ggg::::g  ee::::::::::::ee  r::::rrr:::::::::r  
-  x:::::x  x:::::x    P::::PPPPPP:::::P aaaaaaaaa:::::a g:::::::::::::::::g e::::::eeeee:::::eer:::::::::::::::::r 
-   x:::::xx:::::x     P:::::::::::::PP           a::::ag::::::ggggg::::::gge::::::e     e:::::err::::::rrrrr::::::r
-    x::::::::::x      P::::PPPPPPPPP      aaaaaaa:::::ag:::::g     g:::::g e:::::::eeeee::::::e r:::::r     r:::::r
-     x::::::::x       P::::P            aa::::::::::::ag:::::g     g:::::g e:::::::::::::::::e  r:::::r     rrrrrrr
-     x::::::::x       P::::P           a::::aaaa::::::ag:::::g     g:::::g e::::::eeeeeeeeeee   r:::::r            
-    x::::::::::x      P::::P          a::::a    a:::::ag::::::g    g:::::g e:::::::e            r:::::r            
-   x:::::xx:::::x   PP::::::PP        a::::a    a:::::ag:::::::ggggg:::::g e::::::::e           r:::::r            
-  x:::::x  x:::::x  P::::::::P        a:::::aaaa::::::a g::::::::::::::::g  e::::::::eeeeeeee   r:::::r            
- x:::::x    x:::::x P::::::::P         a::::::::::aa:::a gg::::::::::::::g   ee:::::::::::::e   r:::::r            
-xxxxxxx      xxxxxxxPPPPPPPPPP          aaaaaaaaaa  aaaa   gggggggg::::::g     eeeeeeeeeeeeee   rrrrrrr            
-                                                                   g:::::g                                         
-                                                       gggggg      g:::::g                                         
-                                                       g:::::gg   gg:::::g                                         
-                                                        g::::::ggg:::::::g                                         
-                                                         gg:::::::::::::g                                          
-                                                           ggg::::::ggg                                            
-                                                              gggggg
-															  
-© xPager - xMaps - Manuel Kleinert - www.xpager.ch - info(at)xpager.ch - v 1.1.6 - 19.03.2015
+© xPager - Manuel Kleinert - v 1.1.6 - 23.06.2015
 #####################################################################################################################*/
 
 (function($){
@@ -41,12 +16,13 @@ xxxxxxx      xxxxxxxPPPPPPPPPP          aaaaaaaaaa  aaaa   gggggggg::::::g     e
 var xMaps = function(options) {
 	this.options = $.extend({
 		obj: false,
-        address:false,          // Adress
 		lat:0,                  // Latitude
 		lng: 0,                 // Longitude
+        address:false,          // Adress Position
+        geolocation:false,      // GPS Position
         top: 0,                 // Map move top
         left:0,                 // Map move left
-		zoom: 10,               // Zoom 1-18
+		zoom: 0,                // Zoom 1-18
 		zoomControl: true,      // Zoom Controller
 		scrollWheel: true,      // Map Scroll (Zoom)
         draggable:true,         // Map Drag (Fade)
@@ -55,27 +31,33 @@ var xMaps = function(options) {
         mapType:"ROADMAP",      // Style (HYBRID/ROADMAP/SATELLITE/TERRAIN)
 		streetView: true,       // StreetView Controller
         showMarker:true,        // Show Marker
-        markerIcon: false,      // Add other Icon
+        markerIcon: "",         // Add other Icon
         markerTitle: false,     // Titel
 		markerContent: false,   // Text
         markerOpen:false,       // Text open on start
 		style: false,           // Map Style
         center:false,           // Center Top Left Addition [Top,left]
-        routes:[]               // Route
+        mobileDraggable:false   // Mobile Fade
 	}, options);
 	
     // Options to Attributs
 	for(var name in this.options){eval("this."+name+"=this.options."+name);}
-	
+    
+    // Set Obj
+	if(this.options.obj){
+		this.obj =  this.options.obj[0];	
+	}
+
     // Attributs
 	this.geocoder = new google.maps.Geocoder();
     this.directionsService = new google.maps.DirectionsService();
-	this.location;
+	this.location = false;
 	this.styledMap;
 	this.map;
 	this.mapOptions;
-	this.marker;
-    this.colourArray = ['navy', 'red', 'grey', 'fuchsia', 'black', 'white', 'lime', 'maroon', 'purple', 'aqua', 'green', 'silver', 'olive', 'blue', 'yellow', 'teal'];
+    this.content = "Error";
+	this.markerArray = [];
+    this.ready;
     
     this.init();
     
@@ -83,36 +65,125 @@ var xMaps = function(options) {
 
 // Functions
 xMaps.prototype = {
-	init:function(fx){
-		var self = this;
-		this.setLocation(function(){
-            self.setMap();
-		    self.setStyle();
-		    self.setMarker();
-            self.centerMap();
-            self.generateRequests();
-        })
+    init:function(){
+        var self = this;
         
-		if(fx){fx();}
-	},
+        if(!this.mobileDraggable){
+            if(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+              this.draggable = false;
+            }
+        }
 
-	
-	setMap:function(){
+        this.setLocation(function(){
+            google.maps.event.addDomListener(window,'load',self.setMap()); 
+        });
+    },
+    
+    ready:function(fx){
+        this.ready = fx;
+    },
+
+    setLocation:function(fx){
+        var self = this;
+        
+        // Geo Daten
+        if(this.geolocation && navigator.geolocation){
+            
+            this.getLocationByGeolocation(function(pos){
+                self.location = pos;
+                self.lat = self.location.lat();
+                self.lng = self.location.lng();
+                if(fx){fx();}
+            });
+            
+        // Adresse
+        }else if(this.address){
+            
+            this.getLocationByAdress(this.address,function(pos){
+                self.location = pos;
+                self.lat = self.location.lat();
+                self.lng = self.location.lng();
+                if(fx){fx();}
+            });
+            
+        // Lat und Lng
+        }else{
+            this.getLocationByLatLng(this.lat,this.lng,function(pos){
+                self.location = pos;
+                self.lat = self.location.lat();
+                self.lng = self.location.lng();
+                if(fx){fx();}
+            });
+        }
+    },
+    
+    getLocationByGeolocation:function(fx){
+        navigator.geolocation.getCurrentPosition(function(position){
+            var pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            if(fx){fx(pos);}
+        });
+    },
+    
+    getLocationByAdress:function(address,fx){
+        var self = this;
+        this.geocoder = new google.maps.Geocoder();
+            this.geocoder.geocode({'address':address}
+            , function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    var pos = results[0].geometry.location;
+                }else{
+                    var pos = new google.maps.LatLng(self.lat,self.lng);
+                }
+                if(fx){fx(pos);}
+            });
+    },
+    
+    getLocationByLatLng:function(lat,lng,fx){
+        var self = this;
+        var pos = new google.maps.LatLng(lat,lng);
+        if(fx){fx(pos);}   
+    },
+    
+    setMap:function(){
 		var self = this;
-		// set map options
-		this.mapOptions = {
-			zoomControl: self.zoomControl,
-			panControl: self.panControl,
-			mapTypeControl: self.mapTypeControl,
-			streetViewControl: self.streetView,
-			//travelMode: google.maps.TravelMode.TRANSIT, Evt. Fix
-            draggable:self.draggable,
-			scrollwheel: self.scrollWheel,
-			zoom: self.zoom,
-			center: self.location,
-			mapTypeId: eval("google.maps.MapTypeId."+self.mapType)
+        
+        this.mapOptions = {
+       	    zoomControl:        this.zoomControl,
+			panControl:         this.panControl,
+			mapTypeControl:     this.mapTypeControl,
+			streetViewControl:  this.streetView,
+            draggable:          this.draggable,
+			scrollwheel:        this.scrollWheel,
+            zoom:               this.zoom,
+            center:             this.location,
+            mapTypeId:          eval("google.maps.MapTypeId."+self.mapType)
 		};
+        
         this.map = new google.maps.Map(this.obj,this.mapOptions);
+        
+        // Set Style
+        this.setStyle();
+        
+        // ready
+        
+        if(this.ready){
+            this.ready();
+        }
+        
+        // Set Center Markter
+        this.setMarker({
+            lat:                 this.lat,
+    		lng:                 this.lng,
+            top:                 this.top,
+            left:                this.left,
+            address:             false,
+            showMarker:          this.showMarker,
+            markerIcon:          this.markerIcon,
+            markerTitle:         this.markerTitle,
+    		markerContent:       this.markerContent,
+            markerOpen:          this.markerOpen,
+            disableAutoPan:      false
+        });
 	},
     
     setStyle:function(){
@@ -123,173 +194,92 @@ xMaps.prototype = {
             this.map.setMapTypeId('map_style');
         }
 	},
-	
-	setMarker:function(){
-        if(this.showMarker){
-            var self = this;
-            var markerSetting = new Array();
-            markerSetting['position'] = new google.maps.LatLng(this.lat,this.lng);
-            markerSetting['map'] = this.map;
-            if(this.markerIcon){markerSetting['icon']=this.markerIcon;}
-            if(this.markerTitle){markerSetting['title']=this.markerTitle;}
-            this.marker = new google.maps.Marker(markerSetting);
-        }
-        
-        if(this.markerContent){
-            var infowindow = new google.maps.InfoWindow();
-            infowindow.setContent("<div class='infodiv'>"+ self.markerContent+"</div>");
-            if(this.markerOpen){infowindow.open(self.map,this.marker);}
-            google.maps.event.addListener(this.marker, 'click', function() {
-                infowindow.open(self.map,this);
-            });
-        }
-	},
     
-    removeMarker:function(){
-       this.marker.setMap(null);
+    setMarker:function(options){
+        this.markerArray.push(new xMapsMarker(this,options));
+	}
+}
+
+
+var xMapsMarker = function(obj,options){
+
+    this.options = $.extend({
+        obj:obj,
+        lat:0,
+        lng: 0,
+        top: 0,
+        left:0,
+        address:false,
+        showMarker:true,
+        markerOpen:false,
+        markerIcon: "",
+        markerTitle: false,
+        markerContent: false,
+        disableAutoPan:true
+    }, options);
+    
+    // Options to Attributs
+	for(var name in this.options){eval("this."+name+"=this.options."+name);}
+    
+    this.marker;
+    this.location;
+    
+    this.init();
+}
+
+xMapsMarker.prototype = {
+    
+    init:function(){
+        var self = this;
+
+        this.setLocation(function(){
+            self.setMarker();
+        });  
     },
     
-    setLocation:function(fx) {
+    setLocation:function(fx){
         var self = this;
+        
+        // Adress
         if(this.address){
-            this.geocoder = new google.maps.Geocoder();
-            this.geocoder.geocode({'address': self.address}
-            , function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    self.location = results[0].geometry.location;
-                }else{
-                    self.location = new google.maps.LatLng(self.lat,self.lng);
-                }
-                self.lat = self.location.lat()
-                self.lng = self.location.lng()
-                
+            this.obj.getLocationByAdress(this.address,function(pos){
+                self.location = pos;
                 if(fx){fx();}
             });
-        }else{
-            this.location = new google.maps.LatLng(this.lat,this.long);
-            if(fx){fx();}
+            
+        // Lat und Lng
+        }else if(this.lat && this.lng){
+            this.obj.getLocationByLatLng(this.lat,this.lng,function(pos){
+                self.location = pos;
+                if(fx){fx();}
+            });  
         }
     },
-	
-	centerMap:function(){
-        if(this.center){
-            var self = this;
-            var center = this.location;
-            this.map.setCenter(new google.maps.LatLng(self.lat+(self.top/1000), self.lng-(self.left/1000)));
-        }
-	},
     
-    setRoute:function(route){
-        this.routes.push(route);
-    },
-    
-    generateRequests:function(){
+    setMarker:function(){
+        
         var self = this;
-        var i = 0;
-
-        var checkRoute = function(route,statusIndex,start,end,waypts){
-            
-            if(route.length == statusIndex){
-                var request = {
-                    origin: start,
-                    destination: end,
-                    waypoints: waypts,
-                    optimizeWaypoints: true,
-                    travelMode: google.maps.TravelMode.DRIVING
-                };
         
-                self.directionsService.route(request, function(result, status){
-                    
-                    var directionsRenderer = new google.maps.DirectionsRenderer();
-                    directionsRenderer.setMap(self.map);
-            
-                   /*directionsRenderer.setOptions({
-                        suppressInfoWindows: true,
-                        polylineOptions: {
-                            strokeWeight: 4,
-                            strokeOpacity: 0.8,
-                            strokeColor: self.colourArray[i]
-                        },
-                        markerOptions:{
-                            icon:{
-                                path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-                                scale: 3,
-                                strokeColor: self.colourArray[i]
-                            }
-                        }
-                    });*/
-                    
-                    directionsRenderer.setDirections(result);
- 
-                    i++;
-                });
-            }
-        }
-        
-    
-        $.each(this.routes,function(routeIndex,route){
-            
-            var waypts = [];
-            var start = false;
-            var end = false;
-            var statusIndex = 0;
-            
-            $.each(route,function(index,ort){
-                
-                if(ort["name"]){
-                    
-                    var geocoder = new google.maps.Geocoder();
-                    geocoder.geocode({'address':ort["name"]}, function(results, status) {
-                        // Start
-                        if(index == 0){
-                            if (status == google.maps.GeocoderStatus.OK) {
-                                start = results[0].geometry.location;
-                                statusIndex++;
-                                checkRoute(route,statusIndex,start,end,waypts);
-                            }
-                        }
-                            
-                        // Waypoints
-                        if(index != 0 && index != route.length-1){
-                            if (status == google.maps.GeocoderStatus.OK) {
-                                waypts.push({
-                                    location:results[0].geometry.location,
-                                stopover:true});
-                                statusIndex++;
-                                checkRoute(route,statusIndex,start,end,waypts);
-                            }
-                        }
-                            
-                        // Ende
-                        if(index == route.length-1){
-                            if (status == google.maps.GeocoderStatus.OK) {
-                                end = results[0].geometry.location;
-                                statusIndex++;
-                                checkRoute(route,statusIndex,start,end,waypts);
-
-                                /*var pos = new google.maps.LatLng(end.k,end.D);
-                                
-                                 var marker = new google.maps.Marker({
-                                      position: pos,
-                                      map: self.map
-                                 });    
-        
-                                var infowindow = new google.maps.InfoWindow();
-                                infowindow.setContent("<div class='infodiv'>Test</div>");
-                                infowindow.open(self.map,marker);
-                                google.maps.event.addListener(marker,'click', function() {
-                                    infowindow.open(self.map,this);
-                                });*/
-                                
-                            }
-                        }
-                    
-                        
-                    });
-                }
-            });
+        this.infowindow = new google.maps.InfoWindow({
+            content: "<h3>"+this.markerTitle+"</h3>"+this.markerContent,
+            disableAutoPan: this.disableAutoPan
         });
         
+        this.marker = new google.maps.Marker({
+            position: this.location,
+            icon:this.markerIcon,
+            map: this.obj.map
+        });
+        
+        google.maps.event.addListener(this.marker,'click', function() {
+            self.infowindow.open(self.obj.map,self.marker);
+        });
+        
+        if(this.markerOpen){
+            this.infowindow.open(this.obj.map,this.marker);
+        }
+        
+        this.obj.map.panBy(this.top,this.left);
+        
     }
-
 }
